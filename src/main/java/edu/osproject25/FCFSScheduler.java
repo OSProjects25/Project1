991a99befb3b3bbf2c3f9d85cc136611bdf5477a
Raphael Omorose <raphaelomorose@gmail.com>
2025-09-29 21:03:35 -0400

    Fix variable naming for execution list in FCFSScheduler

--- src/main/java/edu/osproject25/FCFSScheduler.java
+++ src/main/java/edu/osproject25/FCFSScheduler.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FCFSScheduler {
-    private static List<CPUState> ExecutionList = new ArrayList<>();
 
     public static void schedule(List<ProcessObj> p) {
         List<ProcessObj> processes = ProcessSort.byArrivalTime(p);
+        List<CPUState> executionList = new ArrayList<>();
         int time = 0;
 
         for (ProcessObj process: processes) {
             String pName = String.format("P%s", process.getPid());
             time += process.getBurstTime();
             CPUState state = new CPUState(pName, time);
-            ExecutionList.add(state);
+            executionList.add(state);
         }
     }
 }
